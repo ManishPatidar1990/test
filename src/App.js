6e1a8c1ac86d2bc859c6b81df068bda8427b8dbc
manish.patidar@certaintyinfotech.com <55731584+certainty-manishpatidar@users.noreply.github.com>
2021-03-18 16:34:27 +0530

    Update code

--- src/App.js
+++ src/App.js
@@ -7,27 +7,25 @@ class App extends Component {
 
 	constructor(props) {
 		super(props);
-		this.toggleLanguage = () => {
-			this.setState(state => ({
-				lang:
-					state.lang === lan.en
-						? lan.esp
-						: lan.en
-			}));
-		};
-    
 		this.state = {
 			lang: lan.esp,
 			toggleLanguage: this.toggleLanguage
 		};
 	}
 
+
+  toggleLanguage = () => {
+    this.setState(state => ({
+      lang: state.lang === lan.en ? lan.esp : lan.en
+    }));
+  };
+
   render() {
     return (
 		<LanguageContext.Provider value={this.state}>
 			<div className="App">
 				<LanguageContext.Consumer>
-					{({ lang, toggleLanguage }) => (
+					{({ lang }) => (
 						<React.Fragment>
 							<header className="App-header">
 								<h1 className="App-title">{lang.header}</h1>
